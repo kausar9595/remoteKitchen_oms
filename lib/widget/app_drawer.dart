c4633b1d19386b183edafa7d13185f32eebbb89f
nayon <freelancer4747@gmial.com>
2024-03-26 03:19:54 +0600

    appdrader

--- lib/widget/app_drawer.dart
+++ lib/widget/app_drawer.dart
@@ -1,5 +1,10 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
-
+import 'package:get/route_manager.dart';
+import 'package:oms/controller/restaurant_controller.dart';
+import 'package:oms/model/restaurant_model/location_list_model.dart';
+import 'package:oms/widget/app_shemmer.dart';
+import '../model/restaurant_model/restaurantListModel.dart';
 import '../utility/appcolor.dart';
 import '../view/history/history_screen.dart';
 import '../view/menus/menus.dart';
@@ -7,7 +12,8 @@ import '../view/order/screen/orders.dart';
 import '../view/setting_screen/setting_screen.dart';
 
 class AppDrawer extends StatefulWidget {
-  const AppDrawer({super.key});
+  final Widget currentPage;
+  const AppDrawer({super.key, required this.currentPage});
 
   @override
   State<AppDrawer> createState() => _AppDrawerState();
@@ -18,9 +24,57 @@ class _AppDrawerState extends State<AppDrawer> {
   bool _isMenu = false;
   bool _isHistory = false;
 
+  bool _isRestaurantLoading = false;
+  bool _isLocationLoading = false;
+
+  //store restaurant list into a empty list
+  List<RestaurantResult> _restaurantList = [];
+
+  //store location list into a empty list
+  List<LocationResult> _locationList = [];
+
+  //restaurant id
+  String _selectedRestaurantId = "";
+  String _selectedLocationId = "";
+  String selectedValue = "Restaurant Select";
+  String selectedLocationValue = "Location Select";
+
+
+  void _getRestaurantList()async{
+    setState(() => _isRestaurantLoading = true);
+    var res = await RestaurantController.getRestaurantList();
+    for(var i in res.results!){
+      setState(() {
+        _restaurantList.add(i);
+      });
+    }
+    print("_restaurantList == ${_restaurantList[0].id}");
+    setState(() => _isRestaurantLoading = false);
+  }
+
+  void _getLocation()async{
+    setState(() => _isLocationLoading = true);
+    var res = await RestaurantController.getLocationList();
+    for(var i in res.results!){
+      setState(() {
+        _locationList.add(i);
+      });
+    }
+    setState(() => _isLocationLoading = false);
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getRestaurantList();
+  }
+
 
   @override
   Widget build(BuildContext context) {
+
     return Drawer(
       backgroundColor: Colors.white,
       width: MediaQuery.of(context).size.width * 0.30,
@@ -58,86 +112,92 @@ class _AppDrawerState extends State<AppDrawer> {
                 border: Border.all(color: AppColors.textindigo, width: 2),
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: DropdownButton<String>(
-                isExpanded: true,
-                icon: Icon(
-                  Icons.keyboard_arrow_down_rounded,
-                  size: 40,
-                  color: AppColors.textindigo,
-                ),
-                elevation: 0,
-                underline: Container(
-                  decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide.none),
+              child: _isRestaurantLoading ? AppShimmer(width: 200,)
+
+                  : DropdownButtonHideUnderline(
+                child: DropdownButton2<String>(
+                  isExpanded: true,
+                  hint: Text(
+                    'Restaurant Select',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: AppColors.textindigo,
+                    ),
                   ),
+                    items: _restaurantList.map((value) {
+                      return DropdownMenuItem<String>(
+                        value: value.id!.toString(),
+                        child: Text(value.name.toString(),
+                          style: TextStyle(
+                            fontSize: 13
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                    value: _selectedRestaurantId.isNotEmpty ? _selectedRestaurantId : null, // Ensure value is null or non-empty
+                    onChanged: (v) {
+                      setState(() {
+                        _selectedRestaurantId = v!;
+                      });
+                      RestaurantController.addSelectedRestaurantInfo(restaurantId: _selectedRestaurantId.toString()).then((value){
+                        //call location list
+                        _getLocation();
+                      });
+                    },
                 ),
-                hint: Text(
-                  "Restaurant Name",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: AppColors.textindigo,
-                  ),
-                ),
-                items: <String>[
-                  'Restaurant Name',
-                  'Restaurant Name',
-                  'Restaurant Name',
-                ].map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (_) {},
-              ),
+              )
             ),
             SizedBox(
               height: 20,
             ),
             Container(
-              padding: EdgeInsets.only(left: 10,),
-              alignment: Alignment.centerLeft,
-              height: MediaQuery.of(context).size.height * 0.06,
-              width: MediaQuery.of(context).size.width * 0.20,
-              decoration: BoxDecoration(
-                border: Border.all(color: AppColors.textindigo, width: 2),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: DropdownButton<String>(
-                isExpanded: true,
-                icon: Icon(
-                  Icons.keyboard_arrow_down_rounded,
-                  size: 40,
-                  color: AppColors.textindigo,
+                padding: EdgeInsets.only(
+                  left: 10,
                 ),
-                elevation: 0,
-                underline: Container(
-                  decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide.none),
+                alignment: Alignment.centerLeft,
+                height: MediaQuery.of(context).size.height * 0.06,
+                width: MediaQuery.of(context).size.width * 0.20,
+                decoration: BoxDecoration(
+                  border: Border.all(color: AppColors.textindigo, width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: _isLocationLoading ? AppShimmer(width: 200,)
+
+                    : DropdownButtonHideUnderline(
+                  child: DropdownButton2<String>(
+                    isExpanded: true,
+                    hint: Text(
+                      'Location Select',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: AppColors.textindigo,
+                      ),
+                    ),
+                    items: _locationList.map((value) {
+                      return DropdownMenuItem<String>(
+                        value: value.id!.toString(),
+                        child: Text(value.name.toString(),
+                          style: TextStyle(
+                              fontSize: 13
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                    value: _selectedLocationId.isNotEmpty ? _selectedLocationId : null, // Ensure value is null or non-empty
+                    onChanged: (v) {
+                      setState(() {
+                        _selectedLocationId = v!;
+                      });
+                      //After that back to the
+                      Get.to(widget.currentPage, transition: Transition.fade);
+
+                    },
                   ),
-                ),
-                hint: Text(
-                  "Location Name",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: AppColors.textindigo,
-                  ),
-                ),
-                items: <String>[
-                  'Location Name',
-                  'Location Name',
-                  'Location Name',
-                ].map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (_) {},
-              ),
+                )
+
             ),
+
+
             SizedBox(
               height: 30,
             ),
@@ -147,9 +207,11 @@ class _AppDrawerState extends State<AppDrawer> {
                   _isOrder = true;
                   _isMenu = false;
                   _isHistory = false;
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Orders())
-                  );
+                  // Navigator.push(context,
+                  //     MaterialPageRoute(builder: (context) => Orders())
+                  // );
+                  Get.to(Orders(), transition: Transition.rightToLeft);
+
                 });
               },
               child: Container(
@@ -182,7 +244,8 @@ class _AppDrawerState extends State<AppDrawer> {
                   _isMenu = true;
                   _isOrder = false;
                   _isHistory = false;
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Menus()));
+                  //Navigator.push(context, MaterialPageRoute(builder: (context)=>Menus()));
+                  Get.to(Menus(), transition: Transition.rightToLeft);
                 });
               },
               child: Container(
@@ -217,10 +280,7 @@ class _AppDrawerState extends State<AppDrawer> {
                   _isHistory = true;
                   _isOrder = false;
                   _isMenu = false;
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => HistoryScreen()));
+                  Get.to(HistoryScreen(), transition: Transition.rightToLeft);
                 });
               },
               child: Container(
