2a73684799b0545072003ee33c99051f0ebba928
nayon <freelancer4747@gmial.com>
2024-03-26 03:20:46 +0600

    restaurant controller and restaurantListModel.dart

--- lib/view/order/screen/orders.dart
+++ lib/view/order/screen/orders.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:oms/view/order/screen/widget/incoming_order.dart';
 
+import '../../../widget/app_drawer.dart';
+import '../../menus/menus.dart';
+
 
 class Orders extends StatefulWidget {
   const Orders({super.key});
@@ -11,18 +14,23 @@ class Orders extends StatefulWidget {
 }
 
 class _OrdersState extends State<Orders> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("this is order page");
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(child:DefaultTabController(
       length: 3,
       child: Scaffold(
+        drawer: AppDrawer(currentPage: Orders(),),
         appBar: AppBar(
           backgroundColor: Colors.white,
           elevation: 0.5,
-          leading: Padding(
-            padding: const EdgeInsets.only(left: 20.0),
-            child: IconButton(onPressed: (){}, icon: Icon(Icons.menu,size: 30,color: Colors.black,)),
-          ),
           title: Text("Orders",style: TextStyle(fontWeight: FontWeight.w600,color: Colors.black,fontSize: 25),),
           actions: [
             SizedBox(
