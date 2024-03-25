edbf65a4dd91d7ee17a60ff358a99996efb3fe4e
nayon <freelancer4747@gmial.com>
2024-03-26 03:20:21 +0600

    auth check

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:oms/view/auth/check_atuh.dart';
+import 'package:oms/view/menus/menus.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'view/auth/login.dart';
 
@@ -7,20 +10,39 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //check auth
+  var token;
+  getToken()async{
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    setState(() => token = _prefs.getString("token"));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getToken();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'OMS',
       theme: ThemeData(
         fontFamily: 'Poppins',
         primarySwatch: Colors.blue,
       ),
-      home: Login(),
+      home: token == null ? Login() : Menus(),
     );
   }
 }
